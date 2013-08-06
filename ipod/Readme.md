d86ad55c99c8d7f200e39d59fc3cd2789bd892d4
Árni Reynir <arni.reynir89@gmail.com>
2013-08-06 20:01:04 +0000

    ipod

---
+++ ipod/Readme.md
@@ -0,0 +1,11 @@
+#Setup
+- Create virtualenv
+- pip install -r requirements.txt
+
+#Usage
+- Set the source and target in 'ipodImport('source', 'target')'
+- Run python3 main.py
+
+#BUG:
+- If stagger can't read tag then the files is not movied to target
+- If / is in the title of the song, rename will not work.
--- ipod/main.py
+++ ipod/main.py
@@ -6,13 +6,14 @@ import shutil
 ALLOWED_EXTENSIONS = ['mp3','m4a']
 
 def ipodImport(target, source):
+    #Get every directory and files into list.
     lst = os.walk(target)
-
     for (path,dirs, files) in lst:
-
         for f in files:
             if f:
                 pth = os.path.join(path,f)
+
+                #Checks if soundfile is valid.
                 if f[-3:] in ALLOWED_EXTENSIONS:
                     try:
                         tag = stagger.read_tag(pth)
@@ -21,6 +22,7 @@ def ipodImport(target, source):
 
 
                     dest = os.path.join(source, tag.artist)
+                    #Get the format of the file.
                     extension = os.path.splitext(f)[1]
 
                     if tag.album:
@@ -42,13 +44,16 @@ def ipodImport(target, source):
                             renameFile(os.path.join(dest,f), dest, "Unknown",  extension, tag)
 
 
-
+#Renames file if title present else prints error to console.
 def renameFile(source, dest, name, extension,tag):
     try:
+        if tag.track:
+            name = str(tag.track) + ' - ' + str(tag.title)
         os.rename(source, os.path.join(dest,name) + extension)
     except:
         print("Failed rename: " + source)
 
+#Creates folder if not already exist.
 def createFolder(name):
     if not os.path.exists(name):
         os.makedirs(name)
@@ -56,16 +61,3 @@ def createFolder(name):
 
 ipodImport('ipod', 'source')
 
-#list(t)
-#tag.artist
-#tag.album
-#tag.title
-
-
-
-
-#tag = stagger.read_tag('ipod/F00/JDSA.mp3')
-#list(t)
-#tag.artist
-#tag.album
-#tag.title
